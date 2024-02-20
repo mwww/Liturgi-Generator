b306f6430a8410c45dbc80be394eb3827aa4809a
mwww dev <115621609+mwww@users.noreply.github.com>
2024-02-21 00:27:54 +0700

    -

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-3ahiywxl=vox4ouiixvvdy5ywus#-2yn9n2k69i*yaa+5lmm@1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 
 
 # Application definition
@@ -60,7 +60,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.app'
 
 
 # Database
